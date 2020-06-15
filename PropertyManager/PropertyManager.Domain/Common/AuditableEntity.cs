85bb6a0b6bd54c5a6cd1f53f6325088be3a2ca81
Luke <l.norman.dev@gmail.com>
2020-06-15 19:59:02 +0100

    Added base classes
    All the base classes from the erd added.

---
+++ PropertyManager/PropertyManager.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyManager.Domain.Common
+{
+    public abstract class AuditableEntity
+    {
+
+    }
+}
---
+++ PropertyManager/PropertyManager.Domain/ValueTypes/M_Title.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyManager.Domain.ValueTypes
+{
+    public class M_Title
+    {
+    }
+}
